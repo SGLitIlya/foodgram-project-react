0a5afe5615789a0d081a9b3be27824da9dabaf50
Ilya Litvinov <yailay3000@yandex.ru>
2024-04-10 21:06:02 +0300

    PEP8 fix

--- backend/api/v1/pagination.py
+++ backend/api/v1/pagination.py
@@ -2,8 +2,6 @@ from rest_framework.pagination import PageNumberPagination
 
 
 class CustomPageNumberPagination(PageNumberPagination):
-    """Включает изменение размера страницы 
-       путем передачи ограничения в параметрах URL-адреса.
-    """
+    """Включает изменение размера страницы """
 
     page_size_query_param = 'limit'
